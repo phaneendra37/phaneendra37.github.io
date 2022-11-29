f549e9c8c2b4f61707125edf70f0bbaae32871f3
phaneendra37 <kphaneendra337@gmail.com>
2022-11-30 00:53:52 +0530

    Added the action folder

--- actions/deploy.sh
+++ actions/deploy.sh
@@ -16,7 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:phaneendra37//phaneendra37.github.io.git develop:build
+# if you are deploying to https://<USERNAME>.github.io
+ git push -f git@github.com:phaneendra37/phaneendra37.github.io.git master:build
 
 cd -
