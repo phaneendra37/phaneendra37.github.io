669937da5c32ae3a2c7de2edac70d7a33fbc756e
phaneendra37 <kphaneendra337@gmail.com>
2022-11-30 00:40:46 +0530

    Changed the some file names

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Phane</router-link> |
+    <router-link to="/about">Priya</router-link>
   </div>
   <router-view />
 </template>
