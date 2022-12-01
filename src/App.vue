92d2df8d5be417052ed9f4f6764461958470f0c8
phaneendra37 <kphaneendra337@gmail.com>
2022-12-01 23:29:19 +0530

    Added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Phane</router-link> |
-    <router-link to="/about">Priya</router-link>
-  </div>
-  <router-view />
+  <b-container fluid>
+    <div id="nav">
+      <router-link to="/">Phane</router-link> |
+      <router-link to="/about">Priya</router-link>
+    </div>
+    <router-view />
+  </b-container>
 </template>
+<script>
+export default {
+  compatConfig: { MODE: 3 },
+};
+</script>
