b8ea578db5cb24f9b22d0d9bdeb2df922165e96a
phaneendra37 <kphaneendra337@gmail.com>
2024-03-17 04:29:39 +0530

    Added the initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,js,json,html}", "./src/**.{vue,js,json,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
