cd14aa69158ed2057e6c01d84a764f70b878a25a
phaneendra37 <kphaneendra337@gmail.com>
2024-03-22 16:50:57 +0000

    New build was released

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{vue,js,json,html}", "./src/**.{vue,js,json,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
